e9a58405382071fbf415e5020c8fa63a0ed0fbb9
Chetana21 <pchetana691@gmail.com>
2021-10-02 18:19:33 +0530

    different screens

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { Text,StyleSheet,View,TextInput,TouchableOpacity, Dimensions} from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import signin from "./screens/signIn";
+import signup from "./screens/signUp";
+import names from "./screens/notesList";
+import page from "./screens/createNote";
+import note from "./screens/note";
+
+const Stack = createStackNavigator();
+
+export default function App () {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Sign In">
+                <Stack.Screen name="Sign In" component={signin}/>
+                <Stack.Screen name="Sign Up" component={signup}/>
+                <Stack.Screen name="Your Saved Notes" component={names}/>
+                <Stack.Screen name="Create Note" component={page}/>
+                <Stack.Screen name="Add Note" component={page}/>
+                <Stack.Screen name="Your Notes" component={names}/>
+                <Stack.Screen name="Notes" component={names}/>
+                <Stack.Screen name="See Your Note" component={note}/>
+                <Stack.Screen name="Notes List" component={names}/>
+                <Stack.Screen name="Your Notes List" component={names}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
